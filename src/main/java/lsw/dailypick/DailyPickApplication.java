20be12b660484bd1e535abf93f1786fa30a0f565
이선우 <sunn10189@gmail.com>
2025-04-01 19:45:46 +0900

    init

---
+++ src/main/java/lsw/dailypick/DailyPickApplication.java
@@ -0,0 +1,13 @@
+package lsw.dailypick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lsw/dailypick/DailyPickApplicationTests.java
@@ -0,0 +1,13 @@
+package lsw.dailypick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
